14bf21c655a5703fb903f35c415ab59fbaffd82b
James Pier <jpier2012@gmail.com>
2019-04-03 17:15:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma array
   if array.length == 1
     str = array.join
   elsif array.length == 2
-    str = array[0] + " and " + array[1]
+    str = array.join(" and ")
   else
     str = "#{array[0...array.length-1]*", "} and #{array.last}"
   end
