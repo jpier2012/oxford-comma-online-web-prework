65f5289487b8336f8f0ff832bf29537cd8ae9f9f
James Pier <jpier2012@gmail.com>
2019-04-03 16:57:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
-def oxford_comma(array)
-
+def oxford_comma array
+  case array
+  when array.length == 1
+    str = array.to_s
+  when array.length == 2
+    str = array.join(" and ")
+  when array.length >= 3
+    str = "#{array[0...array.length]*","} and #{array.last}"
+  end
 end
