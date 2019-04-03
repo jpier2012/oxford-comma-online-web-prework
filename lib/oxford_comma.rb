334c1a87510e67d919484142cfb77dc70706b6fb
James Pier <jpier2012@gmail.com>
2019-04-03 16:58:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma array
   case array
   when array.length == 1
-    str = array.to_s
+    puts str = array.to_s
   when array.length == 2
-    str = array.join(" and ")
+    puts str = array.join(" and ")
   when array.length >= 3
-    str = "#{array[0...array.length]*","} and #{array.last}"
+    puts str = "#{array[0...array.length]*","} and #{array.last}"
   end
 end
 
