79c0ac4e2d8ffcfe517bc615c72d1d0a73e48f8a
James Pier <jpier2012@gmail.com>
2019-04-03 17:09:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma array
   if array.length == 1
     str = array.to_s
   elsif array.length == 2
-    str = array.join(" and ")
+    str = array[0] + " and " + array[1]
   elsif array.length >= 3
     str = "#{array.take(array.length-1)*", "} and #{array.last}"
   end
